51d154ca8d29bebb4745ba333f0311a822b4bb09
Jittawat <jittawat@sandboxglobal.com>
2019-02-04 16:57:35 +0700

    fix export

--- index.js
+++ index.js
@@ -15,18 +15,4 @@ async function toVideo(imagePath, audioPath, outputPath) {
     });
 }
 
-// imagePath = '/Users/jittawat/Downloads/IMG_2304.JPG';
-// audioPath = '/Users/jittawat/Downloads/cartoon001.wav';
-// outputPath = '/Users/jittawat/Desktop/test3.mp4';
-
-// async function run() {
-//     try {
-//         const a = await toVideo(imagePath, audioPath, outputPath);
-//         console.log(a);
-//     } catch(e) {
-//         console.log(e);
-//     }
-// }
-
-// run();
-export default toVideo;
+module.exports = toVideo;
